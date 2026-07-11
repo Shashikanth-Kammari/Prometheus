fae32756ce53bee8303fa6b8fcb0d0a18ccd5d2c
Shashi <shashikanth0312@gmail.com>
2026-07-11 11:45:39 +0530

    adding

--- node_exporter.sh
+++ node_exporter.sh
@@ -34,6 +34,9 @@ VALIDATE $? "Moving to opt directory"
 sudo dnf install wget -y
 VALIDATE $? "deownloading wget command"
 
+sudo dnf install net-tools -y
+VALIDATE $? "deownloading netstat command"
+
 wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz &>>$LOGFILE
 VALIDATE $? "Downloading Node exporter"
 
